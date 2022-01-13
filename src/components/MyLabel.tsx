e7c0faab45052c6cf176c2d6e06570376d4d6fce
Norberto Krucheski <contact@norbertok.com>
2022-01-13 14:57:15 -0300

    docs creados

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,42 @@
+import './mylabel.css'
+import { AllCaps } from '../stories/components/MyLabel.stories';
+
+export interface MyLabelProps {
+    /**
+     * Este es el mensaje a mostrar en la etiqueta
+     */
+    label: string;
+    /**
+     * Este es el tamaño de la etiqueta
+     */
+    size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+    * Si quiere todo capitalizado
+    */
+    allCaps: boolean;
+    /**
+    * Colores basicos del Label
+    */
+    color: 'primary' | 'secondary' | 'tertiary'
+    /**
+    * Selector de color!
+    */
+    fontColor?: string
+}
+
+export const MyLabel = ({
+    label = "No Label",
+    size = 'normal',
+    allCaps = false,
+    color = 'primary',
+    fontColor
+}: MyLabelProps) => {
+    return (
+        <span 
+        className={` label ${size} text-${color} ${fontColor}`}
+        style={{ color:fontColor }}
+        >
+            {allCaps ? label.toUpperCase() : label}
+        </span>
+    )
+}
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,45 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { MyLabel } from "../../components/MyLabel";
+
+
+export default {
+    title: 'UI/MyLabel', //si le poemos "example" va dentro de ese menu
+    component: MyLabel,
+    argTypes: {
+        size: { control: 'select'},
+        color: {control: 'select'},
+        fontColor: {control: 'color'}
+    }
+} as ComponentMeta<typeof MyLabel>
+
+const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel {...args}/>
+
+export const Basic = Template.bind({}); //crea una copia del template, rompiendo referencia de js con los objetos
+Basic.args = {
+    size: 'normal',
+    allCaps: false //true: capitalizar toda la palabra
+}
+
+
+export const AllCaps = Template.bind({});
+AllCaps.args = {
+    size: 'normal',
+    allCaps: true
+}
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+    size: 'normal',
+    color: 'secondary' //primary | secondary | tertiary
+}
+export const Tertiary = Template.bind({});
+Tertiary.args= {
+    size: 'normal',
+    color: 'tertiary'
+}
+
+export const CustomFontColor = Template.bind({});
+CustomFontColor.args = {
+    size: 'h1',
+    fontColor: '#9e0fe0'
+}
